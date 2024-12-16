2ef0b868c1e8329622da89d1a1a86024bdaccabc
Deborah Foroni <deborah.foroni@gmail.com>
2024-12-16 15:01:48 -0300

    update app.py file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from pages import metas, colaborador, financeiro
 
 # Criar menu de navegação
-menu = st.sidebar.radio("Menu de Navegação", ["Início", "Metas", "Colaborador", "Financeiro"])
+menu = st.sidebar.radio("**Menu de Cadastro**", ["Início", "Metas", "Colaborador", "Financeiro"])
 
 # Exibir a página correspondente
 if menu == "Início":
@@ -17,5 +17,11 @@ elif menu == "Colaborador":
 
 elif menu == "Financeiro":
     financeiro.main()  
+
+# Criar menu de navegação
+menu = st.sidebar.radio("**Menu de Gráficos**", ["Laudos Realizados", "Análise de Voluntários"])
+
+# Criar menu de navegação
+menu = st.sidebar.radio("**Menu de Folha de Pagamento**", ["Folha de Pagamento"])
 
 
